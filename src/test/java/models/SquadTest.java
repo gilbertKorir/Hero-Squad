1acf654d6130f7751528a554001729df7c00e16e
gilbertKorir <jerikorir@gmail.com>
2022-05-02 14:09:39 +0300

    ft: added contain, delete and instance of squad tests

--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -9,28 +9,49 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SquadTest {
+    @AfterEach
+    void tearDown() {
+    }
     private Squad setUpNewSquad() {
         ArrayList<Hero> heroes=new ArrayList<Hero>();
         Hero firsthero = new Hero("Spider Bee",45,"Fuelling", "walking long distance");
         Hero secondthero = new Hero("Melvic monker",30,"Flying", "Innactive during raining");
-//        Hero hero=new Hero();
-//        Hero otherHero=new Hero();
+
         heroes.add(firsthero);
         heroes.add(secondthero);
-        return new Squad("queen",5, "plantiff",heroes);
+        return new Squad("colbox",5, "plantiff",heroes);
     }
     @Test
     public void returnAllInstances_true() throws Exception {
         Squad squad=setUpNewSquad();
         Squad otherSquad=setUpNewSquad();
-        assertEquals(2,Squad.getSquads().size());
+        assertEquals(3,Squad.getSquads().size());
     }
 
+    @Test
+    public void deleteAllSquads() {
+        Squad squad=setUpNewSquad();
+        Squad otherSquad=setUpNewSquad();
+        Squad.clearAll();
+        assertEquals(0,Squad.getSquads().size());
+
+    }
+    @Test
+    public void allSquadsContainedInSquad() throws Exception {
+        Squad squad=setUpNewSquad();
+        Squad otherSquad=setUpNewSquad();
+        assertTrue(Squad.getSquads().contains(squad));
+        assertTrue(Squad.getSquads().contains(otherSquad));
+    }
+    @Test
+    public void check_squad_cause(){
+        Squad squad=setUpNewSquad();
+//        Squad mysquad = new Squad("colbox",5, "plantiff", heroes);
+        assertEquals(true, squad instanceof Squad);
+
+    }
     @BeforeEach
     void setUp() {
     }
 
-    @AfterEach
-    void tearDown() {
-    }
 }
